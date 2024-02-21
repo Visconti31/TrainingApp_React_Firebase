d47991777d46888e6a8f3217225d5d439bbbe71e
Joao Pedro Portella Guedes Visconti <JOVSI@vestas.com>
2024-02-21 11:33:03 +0000

    Start to create the controllers and routes

---
+++ controllers/exerciseControlers.js
@@ -0,0 +1,37 @@
+import firebase from '../firebase.js'
+import Exercise from '../models/exerciseModel.js'
+
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+} from 'firebase/firestore'
+
+const db = getFirestore(firebase)
+
+export const getExercises = async (req, res, next) => {
+  try {
+    const exercises = await getDocs(collection(db, 'exercises'))
+    const exerciseArray = []
+
+    if (exercises.empty) {
+      res.status(400).send('No products found')
+    } else {
+      exercises.forEach((doc) => {
+        const exercise = new Exercise(
+          doc.is,
+          doc.data().name,
+          doc.data().bodyPart,
+          doc.data().exerciseType
+        )
+        exerciseArray.push(exercise)
+      })
+    }
+
+    res.status(200).send(exerciseArray)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
---
+++ models/exerciseModel.js
@@ -0,0 +1,10 @@
+class Exercise {
+  constructor(id, name, bodyPart, exerciseType) {
+    ;(this.id = id),
+      (this.name = name),
+      (this.bodyPart = bodyPart),
+      (this.exerciseType = exerciseType)
+  }
+}
+
+export default Exercise
